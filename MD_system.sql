b8beadc596ee0258f5af263d0a72065ffdc4ab9f
Hyeonseong Jeon <cutz309@gmail.com>
2018-12-29 19:18:50 +0900

    Update

--- MD_system.sql
+++ MD_system.sql
@@ -1,1 +1,3 @@
-CREATE TABLE Book;
+SELECT staff.ename, staff.job
+FROM Emp staff, Emp manager
+WHERE staff.mgr=manager.empno and manager.ename LIKE 'BLAKE';
--- lec01[MD_madang].sql
+++ lec01[MD_madang].sql
@@ -17,3 +17,9 @@ SELECT SUM(saleprice) FROM Orders;
 SELECT custid, COUNT(*) AS 도서수량, SUM(saleprice) AS 총액
 FROM Orders GROUP BY custid ORDER BY custid;
 SELECT * FROM Customer, Orders WHERE Customer.custid=Orders.custid ORDER BY Customer.custid;
+
+SELECT name, SUM(saleprice) FROM Customer, Orders WHERE Customer.custid=Orders.custid 
+GROUP BY customer.name ORDER BY customer.name;
+
+SELECT Customer.name, book.bookname FROM Customer, Orders, Book
+WHERE Customer.custid=Orders.custid AND Orders.bookid=Book.bookid AND Book.price=20000;
